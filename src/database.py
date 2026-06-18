51c8be3e04243e7023933e26f1c3811d7b52e825
Maria Fernanda Lopez <284690333+maferlopezbarra@users.noreply.github.com>
2026-06-18 17:39:47 +0200

    Elevation and Factor costumed parameters

--- src/database.py
+++ src/database.py
@@ -4,10 +4,16 @@ from pathlib import Path
 BASE_DIR: str = Path(__file__).resolve().parent.parent
 
 
-def get_database_path():
+def get_config():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--db", "-d", default="input.db", help="Database file in project/data/input/"
     )
+    parser.add_argument(
+        "--elevation", "-e", default="10300", help="Level 0 elevation", type=float
+    )
+    parser.add_argument(
+        "--factor", "-f", default="250", help="Seismic factor", type=float
+    )
     args: str = parser.parse_args()
-    return BASE_DIR / "data/input" / args.db
+    return {"db": BASE_DIR / "data/input" / args.db, "elevation": args.elevation, "factor": args.factor}
