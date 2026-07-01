042feba80d939fcd86799b038b671e80e91c3f0d
Maria Fernanda Lopez <284690333+maferlopezbarra@users.noreply.github.com>
2026-07-01 20:11:00 +0200

    test API

--- tests/test_api.py
+++ tests/test_api.py
@@ -12,6 +12,17 @@ def test_status():
     assert response.json() == {"status": "ok"}
 
 
+'''def test_input_output():
+    with open("data/input/input.db", "rb") as file:
+        response = client.post(
+            "/displacements",
+            files={"file": ("data/input/input.db", file, "application/octet-stream")},
+            data={"elevation": 10300, "factor": 250},
+        )
+    assert response.status_code == 200
+    assert response.headers["content-type"].startswith("text/csv")
+    assert "attachment" in response.headers["content-disposition"]'''
+
 def test_input_output():
     with open("data/input/input.db", "rb") as file:
         response = client.post(
@@ -23,7 +34,14 @@ def test_input_output():
     assert response.headers["content-type"].startswith("text/csv")
     assert "attachment" in response.headers["content-disposition"]
 
-
+'''def test_file_type():
+    response = client.post(
+        "/displacements",
+        files={"file": ("input.txt", BytesIO(b"txt"), "text/plain")},
+        data={"elevation": 10300, "factor": 250},
+    )
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Only .db files are accepted"}'''
 
 def test_file_type():
     response = client.post(
